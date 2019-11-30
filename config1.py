84017da82b9309bb88e62d281de598033b2bfdc9
Akshat Agarwal <17ucs014@lnmiit.ac.in>
2019-11-30 22:16:27 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -1,3 +1,3 @@
-sid = "ACc2bd47eaaeefdd4152ba8ae2ba25bd15"
-token = "17ecd90a0349215541904926e1db2941"
-number ="+12056193680"
+sid = "enter twilio sid"
+token = "enter twilio token"
+number ="enter twilio number"
